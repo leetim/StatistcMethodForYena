3d75261935f18238166ea5f0b12345ea050679b6
Artem <leetim@live.ru>
2017-02-21 12:14:19 +1000

    Написал алгоритм Левинсона, почти работает но нужно глянуть в чем дело

---
+++ smy.py
@@ -0,0 +1,43 @@
+import matplotlib.pyplot as plt
+import scipy as sp
+import numpy as np
+import random as rnd
+from scipy.optimize import leastsq, linprog
+from scipy import stats
+
+S = np.array(map(lambda x: float(x.split(":")[2]), open("yena.txt", "r").read().split()))
+# print S
+p = 5
+n = len(S)
+# print n
+# C = np.array([[sum([S[k]*S[k + np.abs(i - j)] for k in range(n - np.abs(i - j))])/n for j in range(n)] for i in range(n)])
+# print C
+
+def auto_regr(k , S):
+    return sum(a[i]*S[k - p + i] for i in range(p))
+
+R = [sum([S[j]*S[j + i] for j in range(n - i)])/n for i in range(p+1)]
+print R
+
+a = np.zeros(p+1)
+K = np.zeros(p+1)
+E = np.zeros(p+1)
+E[0] = R[0]
+for l in range(1, p+1):
+    K[l] = (sum(a[i]*R[l - i] for i in range(l)) - R[l])/E[l-1]
+    E[l] = E[l-1]*(1 - K[l]**2)
+    for i in range(1, l):
+        a[i] = a[i] + K[l]*a[i-1]
+    a[l] = -K[l]
+print a
+# print S
+# tau = 7
+c = -sum(sum(a[i]*S[n - i-1-tau] for i in range(p)) for tau in range(n - p))/(n - p) + sum(S[p+i] for i in range(n - p))/(n-p)
+for tau in range(30):
+    print c + sum(a[i]*S[n - i-1-tau] for i in range(p))
+    print S[n - tau-1]
+X = [c - 5 + sum(a[i]*S[n - i-1-tau] for i in range(p)) for tau in range(30)]
+Y = [S[n - tau-1] for tau in range(30)]
+t = [i for i in range(30)]
+plt.plot(t, X, t, Y)
+plt.show()
